a93dccbecf9119802e7af976375491b9912804d4
XiG <qnhat0312@gmail.com>
2026-05-13 16:38:34 +0700

    feat(certificate)#001: Create file structure for certificate module

---
+++ TPS_FullStack.Server/Modules/Admin/DTOs/CertificateDTOs/CertificateDetailDto.cs
@@ -0,0 +1,32 @@
+﻿namespace TPS_FullStack.Server.Modules.Admin
+{
+    public class CertificateDetailDto
+    {
+        public certificateInfo certificateInfo { get; set; }
+        public ICollection<certificateCourses> certificateCourses { get; set; }
+        public ICollection<certificateStudents> certificateStudents { get; set; }
+    }
+
+    public class certificateInfo
+    {
+        public string MaID { get; set; }
+        public string Ten { get; set; }
+        public string Mota { get; set; }
+        public decimal Thoigiansudung { get; set; }
+        public string Donvicap { get; set; }
+    }
+    public class certificateCourses
+    {
+        public string KhoahocID { get; set; }
+        public string Ten { get; set; }
+    }
+    public class certificateStudents
+    {
+        public string MaID { get; set; }
+        public string ChungchiID { get; set; }
+        public string HocvienID { get; set; }
+        public string HocvienTen { get; set; }
+    }
+
+}
+
---
+++ TPS_FullStack.Server/Modules/Admin/DTOs/CertificateDTOs/CertificateGetAllDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TPS_FullStack.Server.Modules.Admin
+{
+    public class CertificateGetAllDto
+    {
+        public string MaID { get; set; }
+        public string Ten { get; set; }
+        public string Mota { get; set; }
+    }
+
+}
+
