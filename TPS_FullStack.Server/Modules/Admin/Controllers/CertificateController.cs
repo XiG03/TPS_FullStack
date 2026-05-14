ab1f793787fc69359e8e3459918ba23a77c94ee8
XiG <qnhat0312@gmail.com>
2026-05-14 16:11:07 +0700

    Teacher branch inital

--- TPS_FullStack.Server/Modules/Admin/Controllers/CertificateController.cs
+++ TPS_FullStack.Server/Modules/Admin/Controllers/CertificateController.cs
@@ -68,7 +68,7 @@ namespace TPS_FullStack.Server.Modules.Admin
             var result = await _certificateService.CertificateStuAcceptAsync(createDto);
             return StatusCode(result.statusCode, result);
         }
-        [HttpPost("Revoke")]
+        [HttpPut("Revoke")]
         public async Task<IActionResult> CertificateStuRevoke(string MaID)
         {
             var result = await _certificateService.CertificateStuRevokeAsync(MaID);
