78c8b858dab7f080f5660777641f3779e653ad0b
XiG <qnhat0312@gmail.com>
2026-05-14 16:41:26 +0700

    feat(teacher)#001: Design teacherInformationDto for Teacher module

---
+++ TPS_FullStack.Server/Modules/Teacher/DTOs/InformationDtos/teacherInformationDto.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SqlTypes;
+
+namespace TPS_FullStack.Server.Modules.Teacher
+{
+    public class teacherInformationDto
+    {
+
+    }
+    public class teacherInfo
+    {
+        public string MaID { get; set; }
+        public string Hoten { get; set; }
+        public string Gioitinh { get; set; }
+        public string Email { get; set; }
+        public string Diachi { get; set; }
+        public string Dienthoai { get; set; }
+    }
+    public class teacherTopics
+    {
+        public string KhoahocID{get; set;}
+        public string TenKhoahoc {get; set;}
+    }
+    public class teacherCourses
+    {
+        public string KhoahocID {get; set;}
+        public string TenKhoahoc {get; set;}
+    }
+    public class teacherSchedules
+    {
+        public string LichhocID {get; set;}
+        public string KhoahocID{get; set;}
+        public string TenKhoahoc {get; set;}
+    }
+}
+
